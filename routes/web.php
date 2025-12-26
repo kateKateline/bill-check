704bcdfd16777b114ad30d68c563aab1584a4ff8
KateKateline <rizkyramadhan12098@gmail.com>
2025-12-26 22:46:04 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BillController;
+use App\Http\Controllers\AnalysisController;
+use App\Http\Controllers\BillHistoryController;
+
+Route::get('/history', [BillHistoryController::class, 'index'])->name('history');
 
 Route::get('/', function () {
     return view('scan');
 });
+
+Route::post('/bill/upload', [BillController::class, 'store'])
+    ->name('bill.upload');
+
+Route::get('/bill/{bill}', [BillController::class, 'show'])
+    ->name('bill.show');
+
+    Route::post('/bill/{bill}/analyze', [AnalysisController::class, 'analyze'])
+    ->name('bill.analyze');
